12410e05f1ae356eb926bf9466293f9d1918baa2
Kira Dinan <kira.dinan@fallingsnowinteractive.ca>
2025-08-02 20:49:11 -0300

    Added runtime and base asemdef

--- Runtime/Currency.cs
+++ Runtime/Currency.cs
@@ -3,79 +3,81 @@ using UnityEngine;
 
 namespace Fsi.Currencies
 {
-    [Serializable]
-    public class Currency<TEnum> : ISerializationCallbackReceiver
-        where TEnum : Enum
-    {
-        public event Action Changed;
-        
-        [HideInInspector]
-        [SerializeField]
-        private string name;
-        
-        public TEnum type;
-        public int amount;
+	[Serializable]
+	public class Currency<TEnum> : ISerializationCallbackReceiver
+		where TEnum : Enum
+	{
+		[HideInInspector]
+		[SerializeField]
+		private string name;
 
-        public Currency()
-        {
-            type = default;
-            amount = 0;
-        }
-        
-        public Currency(TEnum type, int amount)
-        {
-            this.type = type;
-            this.amount = amount;
-        }
+		public TEnum type;
+		public int amount;
 
-        public bool TryCombine(Currency<TEnum> other, out Currency<TEnum> combined)
-        {
-            if (type.Equals(other.type))
-            {
-                combined = new Currency<TEnum>(type, other.amount);
-                return true;
-            }
+		public Currency()
+		{
+			type = default;
+			amount = 0;
+		}
 
-            combined = null;
-            return false;
-        }
+		public Currency(TEnum type, int amount)
+		{
+			this.type = type;
+			this.amount = amount;
+		}
 
-        public void Add(int amount)
-        {
-            this.amount += amount;
-            Changed?.Invoke();
-        }
+		public void OnBeforeSerialize()
+		{
+			name = ToString();
+		}
 
-        public void Add(Currency<TEnum> currency)
-        {
-            if (type.Equals(currency.type))
-            {
-                amount += currency.amount;
-                Changed?.Invoke();
-            }
-            else
-            {
-                Debug.LogError($"Cannot combine currency - {type} with currency - {currency.type}.");
-            }
-        }
+		public void OnAfterDeserialize()
+		{
+		}
 
-        public void Remove(int amount)
-        {
-            this.amount -= amount;
-            this.amount = Mathf.Clamp(this.amount, 0, int.MaxValue);
-            Changed?.Invoke();
-        }
-        
-        public void OnBeforeSerialize()
-        {
-            name = ToString();
-        }
+		public event Action Changed;
 
-        public void OnAfterDeserialize() { }
+		public bool TryCombine(Currency<TEnum> other, out Currency<TEnum> combined)
+		{
+			if (type.Equals(other.type))
+			{
+				combined = new Currency<TEnum>(type, other.amount);
+				return true;
+			}
 
-        public override string ToString()
-        {
-            return $"{type}: {amount}";
-        }
-    }
+			combined = null;
+			return false;
+		}
+
+		public void Add(int amount)
+		{
+			this.amount += amount;
+			Changed?.Invoke();
+		}
+
+		public void Add(Currency<TEnum> currency)
+		{
+			if (type.Equals(currency.type))
+			{
+				amount += currency.amount;
+				Changed?.Invoke();
+			}
+			else
+			{
+				Debug.LogError($"Cannot combine currency - {type} with currency - {currency.type}.");
+			}
+		}
+
+		public void Remove(int amount)
+		{
+			this.amount -= amount;
+			this.amount = Mathf.Clamp(this.amount, 0, int.MaxValue);
+			Changed?.Invoke();
+		}
+
+		public override string ToString()
+		{
+			return $"{type}: {amount}";
+		}
+	}
 }
--- Runtime/Wallet.cs
+++ Runtime/Wallet.cs
@@ -4,140 +4,121 @@ using UnityEngine;
 
 namespace Fsi.Currencies
 {
-    [Serializable]
-    public abstract class Wallet<TEnum, TCurrency> 
-        where TEnum : Enum
-        where TCurrency : Currency<TEnum>, new()
-    {
-        public event Action Changed;
-        
-        [SerializeField]
-        private List<TCurrency> currencies;
-        public List<TCurrency> Currencies => currencies;
+	[Serializable]
+	public abstract class Wallet<TEnum, TCurrency>
+		where TEnum : Enum
+		where TCurrency : Currency<TEnum>, new()
+	{
+		[SerializeField]
+		private List<TCurrency> currencies;
 
-        protected Wallet()
-        {
-            currencies = new List<TCurrency>();
-        }
+		protected Wallet()
+		{
+			currencies = new List<TCurrency>();
+		}
 
-        public Wallet(TCurrency defaultCurrency)
-        {
-            currencies = new List<TCurrency>();
-            Add(defaultCurrency);
-        }
+		public Wallet(TCurrency defaultCurrency)
+		{
+			currencies = new List<TCurrency>();
+			Add(defaultCurrency);
+		}
 
-        public Wallet(TEnum type, int amount)
-        {
-            currencies = new List<TCurrency>();
-            Add(type, amount);
-        }
+		public Wallet(TEnum type, int amount)
+		{
+			currencies = new List<TCurrency>();
+			Add(type, amount);
+		}
 
-        public Wallet(List<TCurrency> currencies)
-        {
-            this.currencies = new List<TCurrency>();
-            foreach (TCurrency currency in currencies)
-            {
-                Add(currency);
-            }
-        }
+		public Wallet(List<TCurrency> currencies)
+		{
+			this.currencies = new List<TCurrency>();
+			foreach (TCurrency currency in currencies) Add(currency);
+		}
 
-        public Wallet(Wallet<TEnum, TCurrency> wallet)
-        {
-            currencies = new List<TCurrency>();
-            foreach (TCurrency currency in wallet.Currencies)
-            {
-                Add(currency);
-            }
-        }
+		public Wallet(Wallet<TEnum, TCurrency> wallet)
+		{
+			currencies = new List<TCurrency>();
+			foreach (TCurrency currency in wallet.Currencies) Add(currency);
+		}
 
-        public bool TryGetCurrency(TEnum type, out TCurrency currency)
-        {
-            foreach (TCurrency c in currencies)
-            {
-                if (c.type.Equals(type))
-                {
-                    currency = c;
-                    return true;
-                }
-            }
-            
-            currency = null;
-            return false;
-        }
-        
-        public void Add(TEnum type, int amount)
-        {
-            if (TryGetCurrency(type, out TCurrency c))
-            {
-                c.Add(amount);
-            }
-            else
-            {
-                TCurrency currency = new()
-                                     {
-                                         amount = amount,
-                                         type = type,
-                                     };
-                currencies.Add(currency);
-            }
-            
-            Changed?.Invoke();
-        }
+		public List<TCurrency> Currencies => currencies;
+		public event Action Changed;
 
-        public void Add(TCurrency currency)
-        {
-            Add(currency.type, currency.amount);
-            Changed?.Invoke();
-        }
+		public bool TryGetCurrency(TEnum type, out TCurrency currency)
+		{
+			foreach (TCurrency c in currencies)
+				if (c.type.Equals(type))
+				{
+					currency = c;
+					return true;
+				}
 
-        public void Add(Wallet<TEnum, TCurrency> wallet)
-        {
-            foreach (TCurrency currency in wallet.Currencies)
-            {
-                Add(currency);
-            }
-        }
+			currency = null;
+			return false;
+		}
 
-        public bool CanAfford(TCurrency currency)
-        {
-            if (TryGetCurrency(currency.type, out TCurrency c))
-            {
-                return c.amount >= currency.amount;
-            }
+		public void Add(TEnum type, int amount)
+		{
+			if (TryGetCurrency(type, out TCurrency c))
+			{
+				c.Add(amount);
+			}
+			else
+			{
+				TCurrency currency = new()
+				                     {
+					                     amount = amount,
+					                     type = type
+				                     };
+				currencies.Add(currency);
+			}
 
-            return false;
-        }
+			Changed?.Invoke();
+		}
 
-        public bool Remove(TCurrency currency)
-        {
-            if (TryGetCurrency(currency.type, out TCurrency c))
-            {
-                if (c.amount < currency.amount)
-                {
-                    return false;
-                }
-                
-                c.Remove(currency.amount);
-                Changed?.Invoke();
-                
-                return true;
-            }
+		public void Add(TCurrency currency)
+		{
+			Add(currency.type, currency.amount);
+			Changed?.Invoke();
+		}
 
-            return false;
-        }
+		public void Add(Wallet<TEnum, TCurrency> wallet)
+		{
+			foreach (TCurrency currency in wallet.Currencies) Add(currency);
+		}
 
-        public override string ToString()
-        {
-            string s = "";
-            foreach (TCurrency currency in currencies)
-            {
-                s += currency.ToString();
-                if (currency == currencies[^1])
-                {
-                    s += "\n";
-                }
-            }
-            return s;
-        }
-    }
+		public bool CanAfford(TCurrency currency)
+		{
+			if (TryGetCurrency(currency.type, out TCurrency c)) return c.amount >= currency.amount;
+
+			return false;
+		}
+
+		public bool Remove(TCurrency currency)
+		{
+			if (TryGetCurrency(currency.type, out TCurrency c))
+			{
+				if (c.amount < currency.amount) return false;
+
+				c.Remove(currency.amount);
+				Changed?.Invoke();
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public override string ToString()
+		{
+			string s = "";
+			foreach (TCurrency currency in currencies)
+			{
+				s += currency.ToString();
+				if (currency == currencies[^1]) s += "\n";
+			}
+
+			return s;
+		}
+	}
 }
